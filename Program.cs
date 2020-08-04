26d1272dcde614836c83de88c64de1d763324083
Daniel Marchi <marchimail@gmail.com>
2020-08-04 09:52:38 -0300

    meu primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
